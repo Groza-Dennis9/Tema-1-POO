a3ff522e8b2a6f6c2650a445c05802cd89f8283e
Groza-Dennis9 <jisatusu.chan99@gmail.com>
2026-03-17 01:44:21 +0200

    Update Tema_1.cpp
    Created the class GameEngine and tested the entire program
    Removed mentions of std:: as the program includes "using namespace std"

--- Tema_1.cpp
+++ Tema_1.cpp
@@ -2,12 +2,11 @@
 #include <cstring>
 using namespace std;
 
-
 class CatCard {
 private:
     int tier;
     bool isRare;
-    char gender; //F or M
+    char gender;
     char* breedName;
     static int totalCatsCreated;
 
@@ -34,26 +33,6 @@ public:
 };
 int CatCard::totalCatsCreated = 0;
 
-void CatCard::deepCopy(const char* name) {
-    if (name) {
-        breedName = new char[strlen(name) + 1];
-        strcpy(breedName, name);
-    }
-    else
-        breedName = nullptr;
-}
-const char* CatCard::getBreedByTier (int t) const {
-    switch (t) {
-        case 1: return "Kitten";
-        case 2: return "Tabby";
-        case 3: return "Bengal";
-        case 4: return "Persian";
-        case 5: return "MaineCoon";
-        case 6: return "Sphinx";
-        default: return "LegendaryCat";
-    }
-}
-
 CatCard::CatCard(int t, bool rare, char g, const char *name) : tier(t), isRare(rare), gender(g) {
     if (name)
         deepCopy(name);
@@ -72,6 +51,26 @@ CatCard::CatCard() {
 }
 CatCard::~CatCard() {
     delete[] breedName;
+}
+
+void CatCard::deepCopy(const char* name) {
+    if (name) {
+        breedName = new char[strlen(name) + 1];
+        strcpy(breedName, name);
+    }
+    else
+        breedName = nullptr;
+}
+const char* CatCard::getBreedByTier (int t) const {
+    switch (t) {
+        case 1: return "Kitten";
+        case 2: return "Tabby";
+        case 3: return "Bengal";
+        case 4: return "Persian";
+        case 5: return "MaineCoon";
+        case 6: return "Sphinx";
+        default: return "LegendaryCat";
+    }
 }
 
 void CatCard::setTier(int t) {
@@ -97,7 +96,7 @@ CatCard& CatCard::operator= (const CatCard& other) {
     return *this;
 }
 ostream& operator<<(ostream& os, const CatCard& c) {
-    os << (c.breedName ? c.breedName : "Unknown") << " [T" << c.tier << "] (" << c.gender << ")";
+    os << "[ "<<(c.breedName ? c.breedName : "Unknown") << " ~ T" << c.tier <<" ~ "<< c.gender << " ]";
     return os;
 }
 istream& operator>>(istream& is, CatCard& c) {
@@ -119,7 +118,7 @@ private:
     CatCard* card;
     float cleanliness;
     int* logHistory;
-    const int slotID;
+    const int spotID;
 
 public:
     //Constructor
@@ -141,17 +140,18 @@ public:
 
     //Operators overload
     ShelterSpot& operator=(const ShelterSpot& other);
-    friend std::ostream& operator<<(std::ostream& os, const ShelterSpot& s);
+    friend ostream& operator<<(ostream& os, const ShelterSpot& s);
 };
 
-ShelterSpot::ShelterSpot(int id) : card(nullptr), slotID(id) {
+ShelterSpot::ShelterSpot(int id) : card(nullptr), cleanliness(100), spotID(id){
     logHistory = new int[3]{0, 0, 0};
 }
-ShelterSpot::ShelterSpot(const ShelterSpot &other) : slotID(other.slotID) {
+ShelterSpot::ShelterSpot(const ShelterSpot &other) : spotID(other.spotID) {
     card = other.card ? new CatCard(*other.card) : nullptr;
     logHistory = new int[3];
     for (int i = 0; i < 3; i++)
         logHistory[i] = other.logHistory[i];
+    cleanliness = other.cleanliness;
 }
 ShelterSpot::~ShelterSpot() {
     delete card;
@@ -197,8 +197,8 @@ ShelterSpot& ShelterSpot:: operator=(const ShelterSpot& other) {
         }
         return *this;
     }
-std::ostream& operator<<(std::ostream& os, const ShelterSpot& s) {
-    os << "Slot " << s.slotID << ": ";
+ostream& operator<<(ostream& os, const ShelterSpot& s) {
+    os << "Slot " << s.spotID << ": ";
     if (s.card) os << *s.card; else os << "Empty";
     os << " | Clean: " << s.cleanliness << "% (History: " << s.logHistory[2] << " cleans)";
     return os;
@@ -225,15 +225,19 @@ public:
     void addFunds(double amount);
 
     //Operator overload
-    friend std::ostream& operator<<(std::ostream& os, const Economy& e);
+    friend ostream& operator<<(ostream& os, const Economy& e);
 };
 
 Economy::Economy(double b, double m) : balance(b), multiplier(m) {}
 
-double Economy::getBalance() const { return balance; }
-void Economy::addFunds(double amount) { balance += (amount * multiplier); }
+double Economy::getBalance() const {
+    return balance;
+}
+void Economy::addFunds(double amount) {
+    balance += (amount * multiplier);
+}
 
-std::ostream& operator<<(std::ostream& os, const Economy& e) {
+ostream& operator<<(ostream& os, const Economy& e) {
     os << "Purr-Points: " << e.balance;
     return os;
 }
@@ -242,12 +246,161 @@ std::ostream& operator<<(std::ostream& os, const Economy& e) {
 
 class GameEngine {
     //running the game
+private:
+    ShelterSpot* spots[6];
+    Economy stats;
+    bool isRunning;
+    int difficulty;
 
+    bool canMergeAny() const;
+    bool isShelterFull() const;
+public:
+    //Constructors
+    GameEngine();
+    GameEngine(int diff);
+    GameEngine(const GameEngine& o);
+    //Destructor
+    ~GameEngine();
+
+    //Functions
+    void spawnCat();
+    void attemptMerge(int a, int b);
+    void cleanSlot(int id);
+
+    void play();
+
+    //Operator overload
+    GameEngine& operator=(const GameEngine& o);
+    friend ostream& operator<<(ostream& os, const GameEngine& ge);
+    friend istream& operator>>(istream& is, GameEngine& ge);
 };
 
+GameEngine::GameEngine() : stats(10.0, 1.0), isRunning(true), difficulty(0) {
+    for (int i = 0; i < 6; ++i) spots[i] = new ShelterSpot(i);
+}
+GameEngine::GameEngine(int diff) : stats(0.0, 1.5), isRunning(true), difficulty(diff) {
+    for(int i=0; i<6; ++i) spots[i] = new ShelterSpot(i);
+}
+GameEngine::GameEngine(const GameEngine& o) : stats(o.stats), isRunning(o.isRunning), difficulty(o.difficulty) {
+    for(int i=0; i<6; ++i) spots[i] = new ShelterSpot(*o.spots[i]);
+}
+GameEngine::~GameEngine() {
+    for (int i = 0; i < 6; ++i) delete spots[i];
+}
+
+bool GameEngine::canMergeAny() const {
+    for (int i = 0; i < 6; ++i) {
+        for (int j = i + 1; j < 6; ++j) {
+            if (!spots[i]->isEmpty() && !spots[j]->isEmpty()) {
+                if (spots[i]->getCard()->getTier() == spots[j]->getCard()->getTier())
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+bool GameEngine::isShelterFull() const {
+    for (int i = 0; i < 6; ++i)
+        if (spots[i]->isEmpty())
+            return false;
+    return true;
+}
+
+void GameEngine::spawnCat() {
+    for (int i = 0; i < 6; ++i) {
+        if (spots[i]->isEmpty() && spots[i]->getCleanliness() > 20.0f) {
+            spots[i]->occupy(CatCard(1, false, 'F'));
+            for(int j=0; j<6; j++)
+                spots[j]->deteriorate();
+            return;
+        }
+    }
+    std::cout << "Cannot rescue! Shelter full or too dirty.\n";
+}
+void GameEngine::attemptMerge(int a, int b) {
+    if (a >= 0 && a < 6 && b >= 0 && b < 6 && a != b && !spots[a]->isEmpty() && !spots[b]->isEmpty()) {
+        if (spots[a]->getCard()->getTier() == spots[b]->getCard()->getTier()) {
+            int nextTier = spots[a]->getCard()->getTier() + 1;
+            spots[a]->occupy(CatCard(nextTier, false, 'M'));
+            spots[b]->evict();
+            stats.addFunds(10.0);
+            for(int j=0; j<6; j++) spots[j]->deteriorate();
+        }
+    }
+    else
+        std::cout << "Merge invalid!\n";
+}
+void GameEngine::cleanSlot(int id) {
+    if (stats.getBalance() >= 5.0) {
+        stats.addFunds(-5.0);
+        spots[id]->clean();
+    } else {
+        std::cout << "Not enough points!\n";
+    }
+}
+
+void GameEngine::play() {
+    while (isRunning)
+    {
+        // std::cout << "================================" << std::endl;
+        // std::cout << "      CAT SHELTER MANAGER       " << std::endl;
+        // std::cout << "================================" << std::endl;
+        // std::cout << stats << std::endl;
+        //idea: clear the screen for each new load
+
+        std::cout << "\n" << stats << "\n";
+        for (int i = 0; i < 6; ++i)
+            std::cout << *spots[i] << "\n";
+        if (isShelterFull() && !canMergeAny()) {
+            std::cout << "\n!!! GAME OVER !!! No more moves possible.\n";
+            isRunning = false;
+        }
+        std::cout << "1:Rescue | 2:Merge | 3:Clean (5pts) | 4:Exit\nChoice: ";
+        int choice; std::cin >> choice;
+
+        switch (choice) {
+            case 1: spawnCat();
+                break;
+            case 2:
+            { int a, b;
+                std::cout << "Enter IDs to merge: ";
+                std::cin >> a >> b;
+                attemptMerge(a, b);
+            }
+                break;
+            case 3:
+            { int id;
+                std::cout << "Enter the ID to clean: ";
+                std::cin >> id;
+                cleanSlot(id);
+            }
+                break;
+            default: isRunning = false;
+        }
+        for (int i=0; i<100; i++) {
+            cout<<endl;
+        }
+    }
+
+}
+
+GameEngine& GameEngine::operator=(const GameEngine& o) {
+    if(this != &o) {
+        for(int i=0; i<6; ++i) delete spots[i];
+        stats = o.stats; isRunning = o.isRunning; difficulty = o.difficulty;
+        for(int i=0; i<6; ++i) spots[i] = new ShelterSpot(*o.spots[i]);
+    }
+    return *this;
+}
+ostream& operator<<(ostream& os, const GameEngine& ge) {
+    return os << ge.difficulty;
+}
+istream& operator>>(istream& is, GameEngine& ge) {
+    return is >> ge.difficulty;
+}
 
 int main() {
-
-
+    GameEngine game;
+    game.play();
     return 0;
 }
